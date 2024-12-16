39c0ffa138d726ba7674cbf82f232ce167454ca7
Marcin Mika <marcin.mika123@gmail.com>
2024-12-16 17:29:11 +0100

    Implement function allowing to change the size of a grid Added range input allowing user to change grid size. Created new addEventListener which get value from range input, and automatically generate new grid.

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
 const container = document.querySelector('#gridContainer');
-
 const btnClear = document.querySelector('#clearBtn');
+const btnEraser = document.querySelector('#eraserBtn');
+const colorPicker = document.querySelector('#colorPicker');
+const gridSizeRange = document.querySelector('#gridSizeRange');
+const gridSizeValue = document.querySelector('#gridSizeValue');
+let isErasing = false;
+let currentColor = '#000000';
 
-btnClear.addEventListener('click', () => clearGrid());
+btnEraser.addEventListener('click', toggleEraser);
+btnClear.addEventListener('click', clearGrid);
+colorPicker.addEventListener('input', changeColor);
 
 function createGridOfSquares(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
+  container.innerHTML = '';
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('box');
     container.appendChild(newDiv);
   }
-}
-
-function changeColorOfDiv() {
-  if (!this.classList.contains('mouseover')) {
-    this.classList.add('mouseover');
-  }
-}
-
-function clearGrid() {
+  const squares = document.querySelectorAll('.box');
   squares.forEach((square) => {
-    square.classList.remove('mouseover');
+    square.addEventListener('mouseover', changeColorOfDiv);
   });
 }
 
-createGridOfSquares(256);
+function changeColorOfDiv() {
+  if (isErasing) {
+    this.style.backgroundColor = '';
+  } else {
+    this.style.backgroundColor = currentColor;
+  }
+}
 
-const squares = document.querySelectorAll('.box');
+function toggleEraser() {
+  isErasing = !isErasing;
+  btnEraser.textContent = isErasing ? 'Drawing' : 'Eraser';
+}
 
-squares.forEach((square) => {
-  square.addEventListener('mouseover', changeColorOfDiv);
+function changeColor() {
+  currentColor = colorPicker.value;
+}
+
+function clearGrid() {
+  const squares = document.querySelectorAll('.box');
+  squares.forEach((square) => {
+    square.style.backgroundColor = '';
+  });
+}
+
+createGridOfSquares(16);
+
+gridSizeRange.addEventListener('input', () => {
+  let gridSize = gridSizeRange.value;
+  gridSizeValue.textContent = `${gridSize} x ${gridSize}`;
+  createGridOfSquares(gridSize);
 });
