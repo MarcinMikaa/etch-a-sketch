8dcae46f03364f5c9a30d2584b10adeb8ed23c2d
Marcin Mika <marcin.mika123@gmail.com>
2024-12-16 15:58:49 +0100

    Implement clear options Added a Clear button with after being clicked remove all things which user draw.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('#gridContainer');
 
+const btnClear = document.querySelector('#clearBtn');
+
+btnClear.addEventListener('click', () => clearGrid());
+
 function createGridOfSquares(gridSize) {
   for (let i = 0; i < gridSize; i++) {
     const newDiv = document.createElement('div');
@@ -12,6 +16,12 @@ function changeColorOfDiv() {
   if (!this.classList.contains('mouseover')) {
     this.classList.add('mouseover');
   }
+}
+
+function clearGrid() {
+  squares.forEach((square) => {
+    square.classList.remove('mouseover');
+  });
 }
 
 createGridOfSquares(256);
